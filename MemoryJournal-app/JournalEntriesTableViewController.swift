fcd221be7e3d896a6d845f4d5337545adf9fd9de
Kendall Poindexter <kendall.poindexter@detroitlabs.com>
2018-09-13 13:39:36 -0400

    Added swipe to delete function

--- MemoryJournal-app/JournalEntriesTableViewController.swift
+++ MemoryJournal-app/JournalEntriesTableViewController.swift
@@ -53,8 +53,20 @@ class JournalEntriesTableViewController: UITableViewController {
         return cell
     }
     
-    //MARK: - Actions
-    
+    //MARK: - Methods
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        func swipeToDelete(indexPath: IndexPath) {
+            journal.entries.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.fade)
+        }
+        
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            swipeToDelete(indexPath: indexPath)
+        }
+        
+    }
+
 
     
     // MARK: - Navigation
