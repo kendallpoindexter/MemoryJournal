4fc445243cd2bcb28b42632c3c47d5213f3fa2a5
Kendall Poindexter <kendall.poindexter@detroitlabs.com>
2018-09-12 21:06:46 -0400

    Created view, controller, and model layers

---
+++ MemoryJournal-app/Journal.swift
@@ -0,0 +1,13 @@
+//
+//  Journal.swift
+//  MemoryJournal-app
+//
+//  Created by Kendall Poindexter on 9/12/18.
+//  Copyright © 2018 Kendall Poindexter. All rights reserved.
+//
+
+import Foundation
+
+struct Journal {
+    let entries: [JournalEntries] = [JournalEntries(title: "Being on IT in The D", content: "Being on IT in The D with Erika, Elle, and Elyse was a lot of fun. My favorite part was our time spent just getting to know one another better.", date: "9/10/2018")]
+}
---
+++ MemoryJournal-app/JournalEntries.swift
@@ -0,0 +1,16 @@
+//
+//  JournalEntries.swift
+//  MemoryJournal-app
+//
+//  Created by Kendall Poindexter on 9/12/18.
+//  Copyright © 2018 Kendall Poindexter. All rights reserved.
+//
+
+import Foundation
+
+struct JournalEntries {
+    var title: String
+    var content: String
+    var date: String
+    
+}
