08821baf53c32a14fbec99e3c2f7a47b06fdb2cc
Kendall Poindexter <kendall.poindexter@detroitlabs.com>
2018-09-13 15:56:22 -0400

    Updated prepare for segue method so that journalentries view controller is populated

---
+++ MemoryJournal-app/JournalEntriesDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  JournalEntriesDetailViewController.swift
+//  MemoryJournal-app
+//
+//  Created by Kendall Poindexter on 9/13/18.
+//  Copyright © 2018 Kendall Poindexter. All rights reserved.
+//
+
+import UIKit
+
+class JournalEntriesDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
